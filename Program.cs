f8319f99e91df8f22db6c29c1aa36ad9636a1344
Denny_07 <kriz.dendropark@hotmail.com>
2025-01-02 08:55:13 +0300

    add some function. ok.

--- Program.cs
+++ Program.cs
@@ -10,45 +10,67 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
-            string answer;
-
-            int result;
+            bool restrt = true;
 
             Console.WriteLine("Hello, welcome to the calculator programm!");
-            Console.WriteLine("Please, enter your first number.");
 
-            num1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please, enter your second number.");
-
-            num2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("What type of operation would you like to do?");
-            Console.WriteLine("Please enter a for addition, s for substraction, m fro multiplication, or d for division.");
-
-            answer = Console.ReadLine();
-
-            if (answer == "a")
+            while (restrt)
             {
-                result = num1 + num2;
-            }
-            else if (answer == "s")
-            {
-                result = num1 - num2;
-            }
-            else if (answer == "m")
-            {
-                result = num1 * num2;
-            }
-            else
-            {
-                result = num1 / num2;
-            }
+
+                int num1;
+                int num2;
+                string answer, input;
+
+                int result;
+
+                Console.WriteLine("Please, enter your first number.");
+
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Please, enter your second number.");
+
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("What type of operation would you like to do?");
+                Console.WriteLine("Please enter a for addition, s for substraction, m fro multiplication, or d for division.");
+
+                answer = Console.ReadLine();
+
+                if (answer == "a")
+                {
+                    result = num1 + num2;
+                }
+                else if (answer == "s")
+                {
+                    result = num1 - num2;
+                }
+                else if (answer == "m")
+                {
+                    result = num1 * num2;
+                }
+                else
+                {
+                    result = num1 / num2;
+                }
 
 
-            Console.WriteLine("The result is " + result);
+                Console.WriteLine("The result is " + result);
+
+                Console.WriteLine("If you want to repeat, put yes, if you want to finish programm, put no.");
+
+                input = Console.ReadLine();
+
+                if (input.ToLower() == "yes")
+                {
+                    restrt = true;
+                }
+                else
+                {
+                    restrt = false;
+                }
+
+            }
+
             Console.WriteLine("Thank you for using the calculator program!!!");
 
             Console.ReadKey();
